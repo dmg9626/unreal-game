91f7d71a6632712b0fa03bb5c144a5c7021dff73
Drew Graham <dmg9626@gmail.com>
2020-11-21 17:08:59 -0500

    merged grapple-redux into main: - recreated grapple character with the better physics - deleted old one (was causing blueprint compile issues) - updated game mode to use new grapple character

--- UnrealProject/Source/UnrealProject/GrappleCharacter.cpp
+++ UnrealProject/Source/UnrealProject/GrappleCharacter.cpp
@@ -1,20 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#define print(text) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 1.5, FColor::Green,text)
-#define printFString(text, fstring) if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Magenta, FString::Printf(TEXT(text), fstring))
+#define print(text) UE_LOG(LogTemp, Warning, TEXT(text))
+#define printFString(text, fstring) UE_LOG(LogTemp, Warning, TEXT(text), fstring)
 #include "GrappleCharacter.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 // Initialize with our custom CharacterMovementComponent
-AGrappleCharacter::AGrappleCharacter(const class FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer.SetDefaultSubobjectClass<UGrappleCharacterMoveComponent>(ACharacter::CharacterMovementComponentName))
+AGrappleCharacter::AGrappleCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
-	// Get pointer to movement component
-	UGrappleCharacterMoveComponent* CharacterMovementComponent = Cast<UGrappleCharacterMoveComponent>(GetCharacterMovement());
-	check(CharacterMovementComponent != nullptr);
-	MovementComponent = CharacterMovementComponent;
 }
 
 // Called when the game starts or when spawned
@@ -25,46 +20,47 @@ void AGrappleCharacter::BeginPlay()
 	PlayerController = GetWorld()->GetFirstPlayerController();
 }
 
-float AGrappleCharacter::GetPitchNormalized()
-{
-	// Get camera rotation
-	FVector position;
-	FRotator rotation;
-
-	PlayerController->GetPlayerViewPoint(position, rotation);
-	return FRotator::NormalizeAxis(rotation.Pitch);
-}
-
 // Called every frame
 void AGrappleCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 }
 
-FVector AGrappleCharacter::CalculateGrappleAxis(FVector vToGrapplePoint)
+FVector AGrappleCharacter::CalculateGrappleForce()
 {
-	FVector axis = FVector::CrossProduct(GetActorForwardVector(), vToGrapplePoint);
-	return axis.GetSafeNormal();
+	// Calculate force of tension
+	FVector grappleTension = CalculateTensionForce();
+
+	// Add forward momentum
+	FVector forwardMomentum = GetActorForwardVector() * ForwardMomentumWeight;
+
+	return grappleTension + forwardMomentum;
 }
 
-bool AGrappleCharacter::IsGrappling()
+FVector AGrappleCharacter::CalculateTensionForce()
 {
-	return MovementComponent->MovementMode == EMovementMode::MOVE_Custom && MovementComponent->CustomMovementMode == static_cast<int>(ECustomMovementType::Swing);
+	// Vector from hook point to player
+	FVector hookToPlayer = GetActorLocation() - GrapplePoint;
+
+	// Calculate dot product with current velocity
+	float dot = FVector::DotProduct(hookToPlayer, GetVelocity());
+
+	// Scale hookToPlayer's length to resulting dot product
+	hookToPlayer = hookToPlayer.GetSafeNormal() * dot;
+
+	// Reverse direction and scale by this force's weight
+	FVector grappleTension = -hookToPlayer;
+	return grappleTension * GrappleTensionWeight;
 }
 
-void AGrappleCharacter::StopGrappling()
-{
-	MovementComponent->SetDefaultMovementMode();
-}
-
-FHitResult AGrappleCharacter::TryGrapple()
+bool AGrappleCharacter::TryGrapple()
 {
 	FHitResult hit(ForceInit);
 
 	// Find/validate player controller instance
 	if (!PlayerController)
 	{
-		return hit;
+		return false;
 	}
 
 	// Initialize raycast values from player perspective
@@ -76,23 +72,21 @@ FHitResult AGrappleCharacter::TryGrapple()
 
 	FCollisionQueryParams traceParams(SCENE_QUERY_STAT(TryGrapple), true, GetInstigator());
 
-	// Perform raycast and return hit result
-	GetWorld()->LineTraceSingleByChannel(hit, traceStart, traceEnd, ECC_Visibility, traceParams);
+	// Perform sphere trace
+	//bool hitReturned = GetWorld()->SweepSingleByChannel(hit, traceStart, traceEnd, traceDirection.Quaternion(), ECC_Visibility, FCollisionShape::MakeSphere(SphereTraceRadius));
+	//if (hitReturned)
+	//{
+	//	FVector GrapplePoint = hit.ImpactPoint;
+	//}
 
-	if (hit.bBlockingHit)
-	{
-		FVector GrapplePoint = hit.Location;
-		//GrappleAxis = CalculateGrappleAxis(traceDirection.Vector());
-		FVector GrappleAxis = GetActorRightVector() * -1;
+	TArray<AActor*> actorsToIgnore{ this };
+	bool hitReturned = UKismetSystemLibrary::SphereTraceSingle(GetWorld(), traceStart, traceEnd, SphereTraceRadius,
+		UEngineTypes::ConvertToTraceType(ECC_Visibility), false, actorsToIgnore, EDrawDebugTrace::ForDuration, hit, true);
+	
+	GrapplePoint = hit.ImpactPoint;
+	IsGrappling = hitReturned;
 
-		// Pass grapple data to movement component
-		MovementComponent->SetGrapple(GrapplePoint, GrappleAxis, hit.Distance);
-
-		// Set movement mode to swinging (will call GrapplePhysCustom() to handle movement)
-		MovementComponent->SetMovementMode(EMovementMode::MOVE_Custom, static_cast<int>(ECustomMovementType::Swing));
-	}
-
-	return hit;
+	return hitReturned;
 }
 
 // Called to bind functionality to input
--- UnrealProject/Source/UnrealProject/GrappleCharacter.h
+++ UnrealProject/Source/UnrealProject/GrappleCharacter.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
-#include "GrappleCharacterMoveComponent.h"
 #include "GrappleCharacter.generated.h"
 
 UCLASS()
@@ -13,41 +12,47 @@ class UNREALPROJECT_API AGrappleCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	//AGrappleCharacter();
-	AGrappleCharacter(const FObjectInitializer& ObjectInitializer);
+	AGrappleCharacter();
+	//AGrappleCharacter(const FObjectInitializer& ObjectInitializer);
 
 	UPROPERTY(EditAnywhere, Category = Grapple);
 	float GrappleRange{ 1000.0f };
 
-	APlayerController* PlayerController;
+	UPROPERTY(EditAnywhere, Category = Grapple);
+	float GrappleTensionWeight{ 2.0f };
 
+	UPROPERTY(EditAnywhere, Category = Grapple);
+	float ForwardMomentumWeight{ 70000.0f };
+
+	UPROPERTY(EditAnywhere, Category = Grapple);
+	float SphereTraceRadius{ 15.0f };
+
+	
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	// Pointer to custom move component
-	class UGrappleCharacterMoveComponent* MovementComponent;
+	UPROPERTY(BlueprintReadWrite, Category = Grapple);
+	bool IsGrappling{ false };
+	
+	APlayerController* PlayerController;
 
-	// Returns camera pitch in degrees, between -180 and 180
-	float GetPitchNormalized();
-
+	UPROPERTY(BlueprintReadOnly, Category = Grapple);
+	FVector GrapplePoint;
+	// Calculates force of grapple tension
+	FVector CalculateTensionForce();
 
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	FVector CalculateGrappleAxis(FVector vToGrapplePoint);
-
-	UFUNCTION(BlueprintCallable, Category = "Grapple")
-	bool IsGrappling();
-
-	UFUNCTION(BlueprintCallable, Category = "Grapple")
-	void StopGrappling();
-
 	/* Returns raycast hit result for a grapple target within GrappleRange in player-facing direction */
 	UFUNCTION(BlueprintCallable, Category = "Grapple")
-	FHitResult TryGrapple();
+	bool TryGrapple();
+
+	UFUNCTION(BlueprintCallable, Category = "Grapple")
+	FVector CalculateGrappleForce();
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
