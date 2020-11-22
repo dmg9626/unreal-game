ba73f6311b5316a93e952152edbae2235fc56042
Drew Graham <dmg9626@gmail.com>
2020-11-22 16:01:27 -0500

    - implemented BouncePad actor class - hooked up bounce logic to grapple character blueprint

---
+++ UnrealProject/Source/UnrealProject/BouncePad.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BouncePad.generated.h"
+
+UCLASS()
+class UNREALPROJECT_API ABouncePad : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABouncePad();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Controls vertical launch force
+	UPROPERTY(EditAnywhere, Category = BouncePad);
+	float VerticalBoost{ 1000.0f };
+
+	// Controls forward launch force
+	UPROPERTY(EditAnywhere, Category = BouncePad);
+	float ForwardBoost{ 1000.0f };
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Returns vector used to launch player
+	UFUNCTION(BlueprintCallable, Category = BouncePad)
+	FVector GetLaunchVector();
+};
